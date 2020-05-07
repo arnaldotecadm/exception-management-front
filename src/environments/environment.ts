1dbd7ab20b48ec657c8dabf73c83afb07cb26138
Arnaldo C. Bezerra <arnaldo-soft-dev@outlook.com>
2020-05-06 22:43:33 -0300

    Correções menores.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: "http://localhost:8383/exception-service",
-  SESSION_API_URL: "http://localhost:8787/session-management-service",
-  API_AUTH: "http://localhost:8080/user-management",
-  SOFTWARE_PUBLIC_KEY: "be9a2bd9-6475-4dcf-b06d-67758af5bce5",
+  API_URL: 'http://localhost:8383/exception-service',
+  SESSION_API_URL: 'http://localhost:8787/session-management-service',
+  API_AUTH: 'http://localhost:8080/user-management',
+  SOFTWARE_PUBLIC_KEY: 'be9a2bd9-6475-4dcf-b06d-67758af5bce5',
 };
