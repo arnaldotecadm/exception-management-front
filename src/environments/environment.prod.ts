e556b5fbbe20995ba0914af69743cf50d404c8d8
Arnaldo C. Bezerra <arnaldo-soft-dev@outlook.com>
2020-06-29 22:28:55 -0300

    Correção de endereços da API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://arcasoftwares.com.br:8383/exception-service',
+  API_URL: 'http://arcasoftwares.zapto.org:9090/exception-service',
   API_AUTH: 'http://arcasoftwares.com.br:8181/user-management',
   SOFTWARE_PUBLIC_KEY: '6a753834-7b33-4610-8657-724118ccfdae',
   SESSION_API_URL: 'http://localhost:8787/session-management-service',
